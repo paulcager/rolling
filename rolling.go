1d3e4c84ec303937d740898f2c2045a3769e2251
Paul Cager <paul.cager@gmail.com>
2017-02-07 01:50:38 +0000

    Slightly more to the skeleton.

--- rolling.go
+++ rolling.go
@@ -1,6 +1,7 @@
 package rolling
 
 import (
+	"sync/atomic"
 	"time"
 )
 
@@ -9,45 +10,63 @@ type Point struct {
 	Value int64
 }
 
-type Aggregator func(point []Point) Point
+type Aggregator func(points []Point) Point
 
 // Got to be a better name than this.
 type Window struct {
-	rows []row
+	rows       []row
 	aggregator Aggregator
+	subs       atomic.Value // of []chan[]Point
 }
 
 type row struct {
 	points []Point
-	next int
+	next   int
 }
 
-func New(aggregator Aggregator, widths []int) *Window {
+func New(aggregator Aggregator, widths ...int) *Window {
 	w := Window{
 		aggregator: aggregator,
-		rows: make([]row, len(widths)),
+		rows:       make([]row, len(widths)),
 	}
-	
+
 	for i := range widths {
 		if widths[i] <= 0 {
 			panic("Widths must be >= 0")
 		}
-		w.rows[i] = row{points:make([]Point, widths[i])}
+		w.rows[i] = row{points: make([]Point, widths[i])}
 	}
-	
+
 	return &w
 }
 
 func (w *Window) Push(p Point) {
-
+	for level := 0; level < len(w.rows); level++ {
+		r := &w.rows[level]
+		r.points[r.next] = p
+		r.next++
+		if r.next < len(r.points) {
+			return
+		}
+		r.next = 0
+		p = w.aggregator(r.points)
+	}
 }
 
 func (w *Window) Flush(level int) {
-
+	r := &w.rows[level]
+	zero := Point{Time: time.Now()}
+	for r.next > 0 {
+		w.Push(zero)
+	}
 }
 
 func (w *Window) Get(level int) []Point {
-
+	r := &w.rows[level]
+	ret := make([]Point, len(r.points))
+	n := copy(ret, r.points[:r.next+1])
+	copy(ret[n:], r.points[n:])
+	return ret
 }
 
 func (w *Window) Close() {
@@ -55,5 +74,26 @@ func (w *Window) Close() {
 }
 
 func (w *Window) Subscribe(level int) chan []Point {
+	panic("TODO")
+}
 
+func Max(points []Point) Point {
+	// Must be at least one element long.
+	max := points[0]
+	for i := 1; i < len(points); i++ {
+		if points[i].Value > max.Value {
+			max = points[i]
+		}
+	}
+
+	return max
+}
+
+func Sum(points []Point) Point {
+	sum := points[0]
+	for i := 1; i < len(points); i++ {
+		sum.Value += points[i].Value
+	}
+
+	return sum
 }
--- rolling_test.go
+++ rolling_test.go
@@ -1,10 +1,41 @@
 package rolling
 
 import (
-    "testing"
+	"testing"
 )
 
-func TestXYZ(t *testing.T) {
+func TestGet(t *testing.T) {
+	w := New(Max, 5, 10)
+	w.Push(Point{Value: 1})
+	w.Push(Point{Value: 2})
+	w.Push(Point{Value: 3})
+	t.Log("First bit")
+	testEquals(t, w.rows[0].points, []int64{1, 2, 3, 0, 0})
+
+	t.Log("Second bit")
+	testEquals(t, w.Get(0), []int64{1, 2, 3, 0, 0})
+
+	w.Push(Point{Value: 4})
+	testEquals(t, w.Get(0), []int64{1, 2, 3, 4, 0})
+
+	w.Push(Point{Value: 5})
+	testEquals(t, w.Get(0), []int64{1, 2, 3, 4, 5})
+
+	w.Push(Point{Value: 6})
+	testEquals(t, w.Get(0), []int64{2, 3, 4, 5, 6})
 
 }
 
+func testEquals(t *testing.T, a []Point, b []int64) bool {
+	if len(a) != len(b) {
+		t.Error(a, b)
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i].Value != b[i] {
+			t.Error(a, b)
+			return false
+		}
+	}
+	return true
+}
